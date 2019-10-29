5dcec0c1997d3f8139ac424fb998156b5b71fc17
Zhehao /Tony/ Lu <tony1023lv@gmail.com>
2019-10-29 16:35:57 -0700

    Added more classes

---
+++ src/main/java/edu/usc/csci310/tUtorSearCher/server/InterceptorConfiguration.java
@@ -0,0 +1,15 @@
+package edu.usc.csci310.tUtorSearCher.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/user/**");
+        super.addInterceptors(registry);
+    }
+}
